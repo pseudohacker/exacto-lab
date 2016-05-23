29ab50736134186085a29a6f246f73f1c87b8348
pseudohacker <pseudohacker@gmail.com>
2016-05-22 22:09:54 -0500

    new!
    new!

--- site/test.php
+++ site/test.php
@@ -10,8 +10,9 @@
 $servername = "localhost";
 $username = "reader";
 $password = "";
-$dbname = "ExactoLab";
+$dbname = "exactolab";
 $charset = "utf8";
+$filter = $_POST["name"];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -21,13 +22,28 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT PK_ITEM, I_DESCRIPCION FROM import_servicios";
+$sql = "SELECT
+	A.`PK_ITEM`,
+	A.`I_DESCRIPCION`,
+	B.`CAT_DESCRIPCION`,
+	LEFT(A.`I_TIEMPO_RESULTADO`,(char_length(A.`I_TIEMPO_RESULTADO`) - 1)) AS `TIEMPO_PROC`,
+	C.`TU_DESCRIPCION`,
+	D.`PK_PRECIO`,
+	D.`PP_NOMBRE`,
+	D.`PP_PRECIO`
+FROM
+		`lista_servicios` A, `servicios_categoria` B, `tiempo_unidad` C, `precios` D
+WHERE
+		A.`I_CATEGORIA`= B.`PK_CAT` AND
+		RIGHT(A.`I_TIEMPO_RESULTADO`,1) = C.`PK_TU` AND
+		A.`PK_ITEM` = D.`FK_ITEM` AND 
+		D.`PP_NOMBRE`='".$filter."' ORDER BY B.`CAT_DESCRIPCION`, A.`I_DESCRIPCION`";
 $result = $conn->query($sql);
-echo("<table><tr><td>ID</td><td>Nombre</td></tr>");
+echo("<table><tr><td>ID</td><td>Nombre</td><td>Precio</td></tr>");
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["PK_ITEM"]. "</td><td>" . $row["I_DESCRIPCION"]. "</td></tr>";
+        echo "<tr><td>" . $row["PK_ITEM"]. "</td><td>" . $row["I_DESCRIPCION"]. "</td><td>" . $row["PP_PRECIO"]. "</td></tr>";
     }
 } else {
     echo "0 results";
---
+++ site/testing2.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is:
+
+</body>
+</html>
+
